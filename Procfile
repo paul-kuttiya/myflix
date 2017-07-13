bafc10fa73670cca1fb9b993c0c6b9a07eeec163
pookzzz2231 <w.kuttiya@gmail.com>
2017-07-13 19:35:54 -0400

    fix unicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
 web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
+
+before_fork do |server, worker|
+   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,10 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+worker_processes 3
 timeout 15
 preload_app true
 
 before_fork do |server, worker|
-   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
-end
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
 
-before_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
     Process.kill 'QUIT', Process.pid
@@ -17,19 +15,18 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
+  end
+
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
-end
-
-after_fork do |server, worker|
-  Sidekiq.configure_client do |config|
-    config.redis = { :size => 1 }
-  end
-  Sidekiq.configure_server do |config|
-    config.redis = { :size => 5 }
-  end
 end
